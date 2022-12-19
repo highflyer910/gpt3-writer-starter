eda46c674827e6ab010488279ee1f8aa7329c2af
highflyer910 <thea.mushambadze@gmail.com>
2022-12-19 23:09:59 +0400

    update

--- pages/index.js
+++ pages/index.js
@@ -4,42 +4,90 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 import { useState } from 'react';
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
-  const onUserChangedText = (event) => {
-  console.log(event.target.value);
-  setUserInput(event.target.value);
+	const [userInput, setUserInput] = useState('');
+	const [apiOutput, setApiOutput] = useState('')
+    const [isGenerating, setIsGenerating] = useState(false)
+	
+	const callGenerateEndpoint = async () => {
+		setIsGenerating(true);
+		console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ userInput }),
+	});
+	
+	const data = await response.json();
+	const { output } = data;
+	console.log("OpenAI replied...", output.text)
+	
+	setApiOutput(`${output.text}`);
+	setIsGenerating(false);
+}
+
+const onUserChangedText = (event) => {
+	console.log(event.target.value);
+	setUserInput(event.target.value);
 };
-  return (
-    <div className="root">
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>MovieMate will tell you what to watch</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Talk with your MovieMate about your mood and movie genre preference</h2>
-          </div>
+
+return (
+<div className="root">
+	<header>
+		<title>MovieMate</title>
+    </header>
+	<div className="container">
+		<div className="header">
+			<div className="header-title">
+            <h1>MovieMate picks movies for you</h1>
+			</div>
+        <div className="header-subtitle">
+            <h2>Chat with MovieMate to discover the perfect movie for your mood and preferences</h2>
+        </div>
         </div>
         {/* Add this code here*/}
         <div className="prompt-container">
-          <textarea
-          placeholder="start typing here"
-          className="prompt-box"
-          value={userInput}
-          onChange={onUserChangedText}
-        />
-        <div className="prompt-buttons">
-          <a class="generate-button" onClick={null}>
-            <div className="generate">
-              <p>Generate</p>
-            </div>
-          </a>
-        </div>
-        </div>
-      </div>
-      
-    </div>
-  );
+			<textarea
+			placeholder="start typing here"
+			className="prompt-box"
+			value={userInput}
+			onChange={onUserChangedText}
+			/>
+			
+		<div className="prompt-buttons">
+			<a
+			className={
+				isGenerating ? "generate-button loading" : "generate-button"
+			}
+			onClick={callGenerateEndpoint}
+			>
+				<div className="generate">
+					{isGenerating ? (
+					<span className="loader"></span>
+					) : (
+					<p>Generate</p>
+					)}
+					</div>
+					</a>
+					</div>
+					{/* New code I added here */}
+					{apiOutput && (
+					<div className="output">
+						<div className="output-header-container">
+							<div className="output-header">
+								<h3>Output</h3>
+							</div>
+						</div>
+						<div className="output-content">
+							<p>{apiOutput}</p>
+						</div>
+					</div>)}
+				</div>
+			</div>
+			
+		</div>
+	);
 };
 
 export default Home;
